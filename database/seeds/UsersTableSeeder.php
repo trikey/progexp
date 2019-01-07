fda91fc18f585019776975be861ea28d47f81b91
ivan <belitskii@gmail.com>
2019-01-08 00:15:05 +0300

    Добавлены настройки webpack, eslint и прочий начальный конфиг

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::firstOrCreate(['email' => 'belitskii@gmail.com'], [
+            'password' => bcrypt('asdasd'),
+            'ia_admin' => 1,
+        ]);
+    }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const webpack = require('webpack');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +14,41 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+  .sourceMaps()
+  .version()
+  .extract([
+    'vue',
+    'moment',
+    'vee-validate',
+    'vue-masked-input',
+    'vue-resource',
+    'lodash',
+    'axios',
+    'jquery',
+  ])
+  .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.webpackConfig({
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    alias: {
+      _components: path.join(__dirname, 'resources', 'assets', 'js', 'components'),
+      _resources: path.join(__dirname, 'resources', 'assets', 'js', 'resources'),
+      _bus: path.join(__dirname, 'resources', 'assets', 'js', 'bus'),
+      _mixins: path.join(__dirname, 'resources', 'assets', 'js', 'mixins'),
+      _services: path.join(__dirname, 'resources', 'assets', 'js', 'services'),
+    },
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru/),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+});
